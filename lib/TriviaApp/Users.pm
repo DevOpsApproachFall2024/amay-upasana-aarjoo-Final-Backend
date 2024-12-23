2f8c2e16cd947aee4623d795de51a16bd92ee80a
AmaySunilShenoyAnagramDS <amaysunilshenoy@anagramds.onmicrosoft.com>
2024-12-23 19:46:01 +0100

    feat: Added auth middleware

--- lib/TriviaApp/Users.pm
+++ lib/TriviaApp/Users.pm
@@ -16,7 +16,30 @@ my $client = MongoDB->connect("mongodb://localhost:27017");
 my $database = $client->get_database('trivia_app');
 my $collection = $database->get_collection('users');
 
+my $pbkdf2 = Crypt::PBKDF2->new(
+    hash_class => 'HMACSHA2',
+    hash_args => {
+        sha_size => 512,
+    },
+    iterations => 10000,
+    salt_len => 16,
+);
 
+# Middleware to check authentication
+hook 'before' => sub {
+    # Skip authentication for login and register routes
+    if (request->path_info =~ m{^/api/users/(login|register)$}) {
+        return;
+    }
+    
+    # Check for protected routes
+    if (request->path_info =~ m{^/api/}) {
+        unless (session('user_id')) {
+            status 401;
+            return halt($json->encode({ error => 'Authentication required' }));
+        }
+    }
+};
 
 
 1;
