8a4de5974d6059d0888563df80c784cd6d9baf38
AmaySunilShenoyAnagramDS <amaysunilshenoy@anagramds.onmicrosoft.com>
2024-12-23 21:46:10 +0100

    feat: added saving scores in results

--- lib/TriviaApp/Quiz.pm
+++ lib/TriviaApp/Quiz.pm
@@ -16,7 +16,7 @@ my $json = JSON::MaybeXS->new(allow_blessed => 1, convert_blessed => 1);
 my $client = MongoDB->connect("mongodb://localhost:27017");
 my $database = $client->get_database('trivia_app');
 my $collection = $database->get_collection('quiz');
-
+my $results = $database->get_collection('results');
 
 prefix '/api/quiz' => sub {
 
@@ -59,7 +59,26 @@ prefix '/api/quiz' => sub {
         return $json->encode(prepare_for_json($question));
     };
 
-    
+    post '/results/:id' => sub {
+        my $email = session('user_id');
+        my $data = from_json(request->body);
+        my $quiz_id = MongoDB::OID->new(value => params->{id});
+
+        my $save_data = {
+            quiz_id => $quiz_id,
+            user_id => $email,
+            score => $data->{score},
+            created_at => time()
+        };
+
+        my $result = $results->insert_one($save_data);
+
+        return $json->encode({
+            success => 1,
+            message => "Results saved successfully",
+            inserted_id => $result->inserted_id
+        });
+    }; 
     
 };
 
